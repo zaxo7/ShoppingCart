7d4286c1379d18d041f8ff64f208bb7c99ac7848
Aghiles Gharbi _ Desktop <zaxo.7@hotmail.com>
2022-05-14 19:28:15 +0100

    fixed bug in file upload

--- admin.php
+++ admin.php
@@ -7,7 +7,7 @@ if(isset($_POST['add_product'])){
    $p_price = $_POST['p_price'];
    $p_image = $_FILES['p_image']['name'];
    $p_image_tmp_name = $_FILES['p_image']['tmp_name'];
-   $p_image_folder = 'uploaded_img/'.$p_image;
+   $p_image_folder = realpath(dirname(__FILE__)).'/uploaded_img/'.$p_image;
 
    $insert_query = mysqli_query($conn, "INSERT INTO `products`(name, price, image) VALUES('$p_name', '$p_price', '$p_image')") or die('query failed');
 
